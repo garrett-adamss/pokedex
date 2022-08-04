82606712fe63fdea871594a88acbe345d3cdd4d6
garrett <garrettadams27@gmail.com>
2022-08-04 09:37:43 -0600

    Active Pokemon Working

--- app/Models/Pokemons.js
+++ app/Models/Pokemons.js
@@ -8,7 +8,8 @@
     
     get Template(){
         return `
-        <div class="text-left m-3 row no-select selectable" onclick="app.pokemonsController.setActivePokemon('${this.url}')">
+        <div class="text-left m-3 row no-select img-fluid selectable row" onclick="app.pokemonsController.setActivePokemon('${this.url}')">
+        <!-- <img class="col-1" src="https://cdn-icons.flaticon.com/png/512/868/premium/868596.png?token=exp=1659626239~hmac=383306841b37c38834f215c2f35a9a30" alt=""> -->
             <h4 class="col-5" >${this.name}</h4>
         </div>
         `
